6a2a1daf1f66b91ad80b82fc25606b2e351e83ea
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2020-10-17 20:36:59 -0300

    criado rotas

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+// importando paginas
+import Home from "./pages/home";
+import Inicial from "./pages/inicial";
+import InicialAdm from "./pages/inicialAdm";
+import TicketAdm from "./pages/ticketAdm";
+import AlteraAdm from "./pages/alteraAdm";
+import AlteraUsuario from "./pages/alteraUsuario";
+import TicketUsuarios from "./pages/ticketUsuarios";
+
+export default function Rota() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/Inicial" exact component={Inicial} />
+        <Route path="/InicialAdm" exact component={InicialAdm} />
+        <Route path="/ticketAdm" exact component={TicketAdm} />
+        <Route path="/AlteraAdm" exact component={AlteraAdm} />
+        <Route path="/alteraUsuario" exact component={AlteraUsuario} />
+        <Route path="/ticketUsuarios" exact component={TicketUsuarios} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
