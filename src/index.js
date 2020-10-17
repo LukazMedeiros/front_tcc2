198b57c9af8b295fd16b3145822b0c5812a5ea09
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2020-10-17 20:35:41 -0300

    inicio do projeto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Rotas from "./routes";
+
+// css
+import "./styles/reset.css";
+import "./styles/base.css";
+import "./styles/grid.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Rotas />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
